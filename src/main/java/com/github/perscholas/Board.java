67cf50b84434fd0a116532954b4473750bc91957
Steven Snyder <sts12589@gmail.com>
2020-07-19 22:09:31 -0400

    added pseudo code and flattened multi to single dimensional array

--- src/main/java/com/github/perscholas/Board.java
+++ src/main/java/com/github/perscholas/Board.java
@@ -1,10 +1,25 @@
 package com.github.perscholas;
 
+import java.util.ArrayList;
+import java.util.*;
+
 /**
  * @author leon on 6/22/18.
  */
 public class Board {
     public Board(Character[][] matrix) {
+//        Make matrix Array into flat one dimensional array
+//        Define clear advantages for X and O characters
+//        Define winner advantages for each character
+
+        List<Character> moves = new ArrayList<Character>();
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j <3 ; j++) {
+                moves.add(matrix[i][j]);
+            }
+        }
+        System.out.println(moves);
     }
 
     public Boolean isInFavorOfX() {
